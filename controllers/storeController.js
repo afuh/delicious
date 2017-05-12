41a6a6cb5c11a60617a8588f3ff5d858da9bca9c
Axel <axelfuh@gmail.com>
2017-05-12 15:21:23 +0200

    Add edit stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -12,11 +12,31 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
-  req.flash('success', `Successfully created ${store.name}!`);
+  req.flash('success', `Successfully created <strong>${store.name}</strong>`);
   res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStore = async (req, res) => {
+  //Query the DB for a list of all stores
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store given the id
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. confirm they are the owner of the store
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // find and update store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id}, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
+  // redirect them to the store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`);
 };
