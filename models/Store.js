c466e3de490e6aa39a135c55b7592eed8375253a
Axel <axelfuh@gmail.com>
2017-05-13 15:16:33 +0200

    Add Tags section

--- models/Store.js
+++ models/Store.js
@@ -3,7 +3,6 @@ const slug = require('slugs');
 
 mongoose.Promise = global.Promise;
 
-
 const storeSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -37,13 +36,27 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this funciton
   }
   this.slug = slug(this.name);
+  // find other stores with the same name
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx});
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`
+  }
   next();
 });
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 }}
+  ]);
+}
 
 module.exports = mongoose.model('Store', storeSchema);
--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,9 @@ router.post('/add/:id',
 );
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoreByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
 
 module.exports = router;
