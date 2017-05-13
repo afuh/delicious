7ac5e0cec520dde15b4be2db4cb38feef94f423b
Axel <axelfuh@gmail.com>
2017-05-13 15:42:36 +0200

    Add Tags section

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -85,7 +85,12 @@ exports.getStoreBySlug = async (req, res, next) => {
 }
 
 exports.getStoreByTag = async (req, res) => {
-  const tags = await Store.getTagsList();
-  const tag = req.params.tag
-  res.render('tag', { tags, title: 'Tags', tag })
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true }
+  
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+
+  res.render('tag', { tags, title: 'Tags', tag, stores })
 }
