bf9ff4ce4226495bd92157a8877bed2c86d4b6e1
Axel <axelfuh@gmail.com>
2017-05-15 16:23:17 +0200

    Add text index

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -102,3 +102,19 @@ exports.getStoreByTag = async (req, res) => {
 
   res.render('tag', { tags, title: 'Tags', tag, stores })
 }
+
+exports.searchStores = async (req, res) => {
+  const stores = await Store
+    // first find stores that match
+    .find(
+      { $text: { $search: req.query.q } },
+      { score: { $meta: 'textScore' } }
+    )
+    //then sort them
+    .sort(
+      { score: { $meta: 'textScore' } }
+    )
+    // limit to only 5 results
+    .limit(5)
+  res.json(stores)
+}
--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,10 @@ router.post('/account/reset/:token',
   catchErrors(authController.update)
 );
 
+/*
+  API
+*/
 
-
+router.get('/api/search', catchErrors(storeController.searchStores));
 
 module.exports = router;
