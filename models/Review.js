6280303509e951cd72872e2ea9015ab44838fe64
Axel <axelfuh@gmail.com>
2017-05-18 12:41:21 +0200

    Add Reviews

---
+++ models/Review.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const reviewSchema = new mongoose.Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    red: 'User',
+    required: 'You must supply an author!'
+  },
+  store: {
+    type: mongoose.Schema.ObjectId,
+    red: 'Store',
+    required: 'You must supply a store!'
+  },
+  text: {
+    type: String,
+    required: 'Your review must have text!'
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5
+  }
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
